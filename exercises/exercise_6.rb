ed03368545ea3d49ad211e343ee98492f4d4bb1f
Esha Roda <esha.roda@gmail.com>
2017-08-29 20:45:28 +0000

    modifying exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,6 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# We haven't used the Employee class (and employees table) at all yet.
+# If you look at this table's column structure, you'll find that it has a `store_id` (integer) column.
+# This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
